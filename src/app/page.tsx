50d17cc42d84d62cc0890b4c11740b01b39039f7
Chandan Shankar <chandan.shankar98@gmail.com>
2025-01-20 21:33:57 +0530

    Sheets page Frontend properly layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,20 +10,22 @@ import { AppStatusBar } from '@/components/spreadsheet/appstatus-bar';
 export default function Page() {
   return (
     <SpreadsheetProvider>
-      <div className="flex flex-col h-screen bg-white">
+      <div className="flex flex-col h-[calc(100vh-32px)] bg-white">
         {/* Header */}
-        <div className="flex flex-col border-b border-gray-200">
+        <div className="flex flex-col shrink-0">
           <ProjectBar />
-          <Toolbar />
+          <div className="mx-4 mb-1">
+            <Toolbar />
+          </div>
         </div>
 
         {/* Spreadsheet Grid */}
-        <div className="flex-1 overflow-auto">
+        <div className="flex-1 relative min-h-0">
           <Spreadsheet />
         </div>
 
         {/* Footer */}
-        <div className="border-t border-gray-200">
+        <div className="shrink-0">
           {/* Sheet Tabs */}
           <div className="h-7 bg-[#F8FCF9] flex items-center">
             <SheetsBar />
--- src/components/spreadsheet/index.tsx
+++ src/components/spreadsheet/index.tsx
@@ -4,8 +4,8 @@ import { Grid } from './grid';
 
 export function Spreadsheet() {
   return (
-    
+    <div className="absolute inset-0 overflow-auto">
       <Grid />
-    
+    </div>
   );
 }
